96c4218c356a6c54dfe75835b0ec7f39b3f4fdcc
Ivan Remen <bhyk@yandex.ru>
2013-02-19 14:04:03 +0400

    moduleInfo.h is now safe to multitimes includes

--- moduleInfo.h
+++ moduleInfo.h
@@ -1,3 +1,8 @@
+#ifndef _BHYCHIK_moduleInfo_H_
+#define _BHYCHIK_moduleInfo_H_
+
 #define THIS_MODULE_AUTHOR "Ivan Remen <bhyk@yandex.ru>"
 #define THIS_MODULE_DESC "This module logs processes attempts to access user stack"
 #define THIS_MODULE_LICENSE "GPL"
+
+#endif
