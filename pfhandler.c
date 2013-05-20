e85f00893433555a94617dcb4a249a6da7f16bae
Ivan Remen <bhyk@yandex.ru>
2013-05-20 23:06:08 +0400

    Correctly detects stack addresses

--- pfhandler.c
+++ pfhandler.c
@@ -1,11 +1,13 @@
 #include "pfhandler.h"
 
+#define DEFAULT_STACK_BOTTOM 0xbf671F00
+
 static pfhook_t oldHook;
 
 void pfHandler(unsigned long address, unsigned long errorcode)
 {
 	pid_t curpid = current->pid;
-	if(((errorcode & 5) == 4) && (isObserved(curpid) == ProccessIsWatched))
+	if(((errorcode & 5) == 4) && (isObserved(curpid) == ProccessIsWatched) && address >= DEFAULT_STACK_BOTTOM)
 	{
 		printk(KERN_INFO "STACKWATCH: pagefault at %lu for %d proccess\n", address, curpid);
 	}
